6f9931176a2c09ef8e02fa0bc713c27b38c0a5d2
Sein Tao <sein.tao@gmail.com>
2015-01-27 12:20:54 +0800

    parse tdx trades use pandas

--- parse_tdx_trades.py
+++ parse_tdx_trades.py
@@ -9,46 +9,55 @@ Created on Sun Jan 25 20:23:32 2015
 import io
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime as dt
 import security
 import trades
 import collections
-#import sys
-#reload(sys)
-#sys.setdefaultencoding('utf8')
+import sys
+pd.set_option('display.encoding', 'utf8')
+
+
 file_encode = 'gb2312'
 codec = 'utf8'
 datefmt, timefmt = "%Y%m%d","%H:%M:%S"
-bs_state = {u'买入'.encode(codec): 'B', u'卖出'.encode(codec): 'S'}
+bs_state = {u'买入': 'Buy', u'卖出': 'Sell'}
 def get_market(account):
     if account[0] == 'A':
         return 'SS'
     elif account[0] == '0':
         return 'SZ'
 #证券名称,成交日期,成交时间,买卖标志,成交价格,成交数量,成交金额,成交编号,委托编号,证券代码,股东代码
-header = ['Name', 'Date', 'Time', 'BS', 'Price', 'Volume', 'Amount',
+RawHeader = ['Name', 'Date', 'Time', 'BS', 'Price', 'Volume', 'Amount',
           'TradeNo','OrderNo', 'Code', 'Account']
-RawRecord = collections.namedtuple('RawRecord', header)
-
-def _toRecord(self):
-    date = datetime.strptime(self.Date + self.Time, datefmt+timefmt)
-    BS = bs_state.get(self.BS, self.BS)
-    code = security.Security(self.Code, self.Name, get_market(self.Account))
-    return trades.TradeRecord(code,BS,float(self.Price), int(self.Volume), date)
-
+RawRecord = collections.namedtuple('RawRecord', RawHeader)
 RawRecord.toRecord = _toRecord
+def _toRecord(self):
+    date = dt.combine(dt.strptime(self.Date,datefmt).date(),
+            dt.strptime(self.Time,timefmt).time())
+    BS = bs_state.get(self.BS, self.BS)
+    #market = get_market(self.Account)
+    code = security.Security(self.Code, self.Name, get_market(self.Account))
+    return [code, date, BS, self.Price, self.Volume]
+
+def _raw(self):
+    return self
+
+header = ['Code', 'Date', 'BS', 'Price', 'Volume']
+
+
+def _parse_file(path):
+    with io.open(path, 'r') as f:
+        f.readline() # header line
+        for line in f.readlines():
+            cols = (x.strip("=\"") for x in line.rstrip().split("\t"))
+            yield RawRecord(*cols).toRecord()
+
 
 def parse_file(path):
-    f = io.open(path, 'r',encoding=file_encode)
-    f.readline() # header line
-    for line in f.readlines():
-        cols = (x.strip("=\"") for x in line.encode(codec).rstrip().split("\t"))
-        yield RawRecord(*cols).toRecord()
-
+    data = pd.DataFrame(_parse_file(path), columns = header)
+    return data
 
 if __name__ == '__main__':
     path = "D:\\Personal\\Finnance\\Stock\\wt.xls"
-    for record in parse_file(path):
-        print record
-
+    data = parse_file(path)
 
